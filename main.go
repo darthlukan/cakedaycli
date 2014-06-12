a491a8fcbf6fd44e38cc00bfc02876d29c1e91d2
Brian Tomlinson <brian@smartm.no>
2014-06-12 21:23:35 +0200

    version 1.0

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "findcakeday"
 	app.Usage = "Find your Reddit Cake Day"
+	app.Version = "1.0"
 	app.Action = func(c *cli.Context) {
 		username := c.Args()[0]
 		response, err := cakeday.Get(username)
